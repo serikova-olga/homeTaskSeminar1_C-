5dd50cf6d20bf51956578476d6cd61f0686166d8
OLGA <serikova-olga@mail.ru>
2022-06-13 19:17:41 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("input number 1: ");
+String input = Console.ReadLine ();
+int number1=Convert.ToInt32(input);
+Console.WriteLine ("input number 2: ");
+String input = Console.ReadLine ();
+int number2=Convert.ToInt32(input);
+
+if (number1>number2)
+{
+    Console.WriteLine("max="+ number1);
+}
+else
+{
+    Console.WriteLine("max="+ number2);
+}
