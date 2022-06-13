4055acf9aa502645a3cca9ce67af4ac49d0d6fcf
OLGA <serikova-olga@mail.ru>
2022-06-13 19:51:02 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("input number : ");
+int number = Convert.ToInt32(Console.ReadLine ());
+int result1 = number/2;
+int result2 = number%2;
+if (result1==result2)
+{
+    Console.WriteLine(number + " - четное");
+}
+else
+{
+   Console.WriteLine(number + " - нечетное"); 
+}
